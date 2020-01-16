04a77738d5af98ce8a5ccd5907f585ac39b2d7ba
Russell Strauss <russellstrauss@gmail.com>
2020-01-15 21:43:57 -0500

    Add build

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// var Scene = require('./components/scene.js');
+// var UI = require('./components/ui.js');
+// var Utilities = require('./utils.js');
+// var Beats = require('./beats.js');
+// var Graphics = require('./graphics.js');
+
+// (function () {
+	
+// 	document.addEventListener('DOMContentLoaded',function(){
+
+// 		Scene().init();
+// 		UI().init();
+// 	});
+// })();
